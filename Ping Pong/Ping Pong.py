d395d0af008e7778bd304985d9231af68ff23ce5
MMish888 <105156641+MMish888@users.noreply.github.com>
2022-05-15 23:22:12 +0300

    Update Ping Pong.py

--- Ping Pong/Ping Pong.py
+++ Ping Pong/Ping Pong.py
@@ -5,11 +5,6 @@ font.init()
 #Окно игры
 clock = time.Clock()
 FPS = 100
-x1 = 550
-y1 = 350
-x2 = 150
-y2 = 350
-step = 10
 window = display.set_mode((1000, 500))
 display.set_caption("Maze")
 background = transform.scale(image.load("background.png"), (1000, 500))
@@ -26,7 +21,7 @@ class GameSprite(sprite.Sprite):
      def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))  
 
-class Player(GameSprite):
+class Player1(GameSprite):
     def update(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
@@ -34,24 +29,19 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y > 5:
             self.rect.y += self.speed
 
-class Enemy(GameSprite):
-    direction = 'left'
-    a = "right"  
+class Player2(GameSprite):
     def update(self):  
-        if self.direction == "left":
-            self.rect.x -= self.speed
-        elif self.direction != "left":
-            self.rect.x += self.speed
-        if self.rect.x >= 800:
-            self.direction = "left"
-        if self.rect.x <= 400:
-            self.direction = "right"
+        keys = key.get_pressed()
+        if keys[K_o] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_l] and self.rect.y > 5:
+            self.rect.y += self.speed
 
         
 #Объекты
-Wizard = Player("Player 1.png", 150, 350, 5 )
-#Rogue = Enemy("Rogue.png", 550, 350, 5 ) 
-font = font.Font(None, 70)
+Player01 = Player1("Player 1.png", 150, 350, 5 )
+Player02 = Player2("Player 2.png", 550, 350, 5 ) 
+font = font.SysFont("Arial", 40)
 win = font.render('YOU WIN!', True, (255, 215, 0))
 lose = font.render('GAME OVER!', True, (255, 215, 0))
 
@@ -61,16 +51,17 @@ finish = False
 while game:
     clock.tick(FPS)
     
-
     display.update()
     for e in event.get():
         if e.type == QUIT:
             game = False
     if finish != True:
         window.blit(background,(0, 0)) 
-        Wizard.update()
+        Player01.update()
+        Player02.update()
 
-        Wizard.reset()
+        Player01.reset()
+        Player02.reset()
 
 
 
